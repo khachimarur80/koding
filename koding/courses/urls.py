8e1905d63cddf388f040babfd419e842a8f91d3f
keirh80 <103277024+keirh80@users.noreply.github.com>
2023-09-07 20:52:59 +0200

    User Authentication
    - Basic User Authentication
    - Profile Model and View
    - Login, Logout and SignUp views

--- koding/courses/urls.py
+++ koding/courses/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("course/<int:course_id>/", views.course, name="course"),
+    path("profile/<str:username>/", views.profile, name="profile"),
+    path("login/", views.login_view, name="login"),
+    path("signup/", views.signup_view, name="signup"),
+    path("logout/", views.logout_view, name="logout"),
 ]
--- koding/courses/views.py
+++ koding/courses/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
-from .models import Course
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Course, Profile
 
 def index(request):
     courses_list = Course.objects.all()
-    context = {"courses_list": courses_list}
+    context = {
+    	"courses_list": courses_list,
+    	"user": request.user,
+    	}
     return render(request, "courses/index.html", context)
 
 
@@ -13,3 +19,39 @@ def course(request, course_id):
     except Course.DoesNotExist:
         raise Http404("Question does not exist")
     return render(request, "courses/course.html", {"course": course})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')
+        else:
+            return render(request, 'courses/login.html', {'error_message': 'Invalid login credentials'})
+    else:
+        return render(request, 'courses/login.html')
+
+def logout_view(request):
+    logout(request)
+    return redirect('index')
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')  # Replace 'signin' with the URL name of your sign-in view
+    else:
+        form = UserCreationForm()
+    return render(request, 'courses/signup.html', {'form': form})
+
+def profile(request, username):
+    try:
+    	user = User.objects.get(username=username)
+    	profile = Profile.objects.get(user=user)
+    except Course.DoesNotExist:
+        raise Http404("User does not exist")
+    return render(request, "courses/profile.html", {"profile": profile})
