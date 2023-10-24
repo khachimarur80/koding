620ae0ba1fd9eefd879b89547340449cd65f4d03
keirh80 <khachimarur80@gmail.com>
2023-10-24 11:21:42 +0200

    Update urls.py

--- koding/koding/urls.py
+++ koding/koding/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("", include("courses.urls")),
