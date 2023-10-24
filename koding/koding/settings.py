21be8b53f4c787c366b4361cfd033f8846af2990
keirh80 <khachimarur80@gmail.com>
2023-10-24 11:25:30 +0200

    Update settings.py

--- koding/koding/settings.py
+++ koding/koding/settings.py
@@ -144,10 +144,10 @@ STATIC_ROOT = 'var/static_root/'
 STATICFILES_DIRS = ['static']
 """
 
-STATIC_URL = '../static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "../static")
+    os.path.join(BASE_DIR, "/static")
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
