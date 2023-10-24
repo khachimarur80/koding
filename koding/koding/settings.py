2fddb117696e0e86c2457b611e5a3297082e37f1
keirh80 <khachimarur80@gmail.com>
2023-10-24 12:03:08 +0200

    Database migrations

--- koding/koding/settings.py
+++ koding/koding/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["127.0.0.1", "https://koding.up.railway.app", "koding.up.railway.app"]
 
-CSRF_TRUSTED_ORIGINS = ["127.0.0.1", "https://koding.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ["https://koding.up.railway.app"]
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
